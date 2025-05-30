532444406ce56835a48156ce249d41c1475e9962
EpicFn <rlagurwls056@gmail.com>
2025-05-30 15:15:17 +0900

    refactoring

--- src/main/java/com/back/Calc.java
+++ src/main/java/com/back/Calc.java
@@ -4,15 +4,14 @@ package com.back;
 public class Calc {
 
     static public int run(String s) {
-        return calLevel1(calLevel0(s));
+        return calRecursion(removeBracket(s), "\\+");
     }
 
     // 괄호 처리
-    static public String calLevel0(String s){
+    static public String removeBracket(String s){
         StringBuilder sb = new StringBuilder();
         StringBuilder buf = new StringBuilder();
         int depth = 0;
-        String op = "";
 
         for(String c : s.split("")){
             if (c.equals("(")){
@@ -26,9 +25,10 @@ public class Calc {
                     buf.append(c);
                 else if(depth == 0){
                     if(buf.toString().contains("("))
-                        sb.append(calLevel0(buf.toString()));
+                        sb.append(removeBracket(buf.toString()));
                     else
-                        sb.append(String.valueOf(calLevel1(buf.toString())));
+                        sb.append(calRecursion(buf.toString(), "\\+"));
+
                     buf = new StringBuilder();
                 }
             }
@@ -42,32 +42,11 @@ public class Calc {
         return sb.toString();
     }
 
-    // plus 처리
-    static public int calLevel1(String s){
-        String[] parts = s.split( " \\+ ");
+    // 사칙 연산 통합
+    static public int calRecursion(String str, String op){
+        String[] parts = str.split( " " + op + " ");
         int result = 0;
-        int buf;
-        boolean plusFlag = true;
-
-        for(String p : parts){
-            try{
-                buf = Integer.parseInt(p.trim());
-                result += buf;
-
-            }catch(Exception e){
-                buf = calLevel2(p);
-                result += buf;
-            }
-        }
-
-        return result;
-    }
-
-    // minus 처리
-    static public int calLevel2(String s) {
-        String[] parts = s.split(" - ");
-        int result = 0;
-        int buf;
+        int buf = 0;
         boolean flag = true;
 
         for(String p : parts){
@@ -77,55 +56,36 @@ public class Calc {
                     result = buf;
                     flag = false;
                 }
-                else
-                    result -= buf;
+                else{
+                    switch(op){
+                        case "\\+" -> result += buf;
+                        case "-" -> result -= buf;
+                        case "\\*" -> result *= buf;
+                    }
+                }
 
             }catch(Exception e){
-                buf = calLevel3(p);
+                switch(op){
+                    case "\\+" -> buf = calRecursion(p, "-");
+                    case "-" -> buf = calRecursion(p, "\\*");
+                }
+
                 if(flag){
                     result = buf;
                     flag = false;
                 }
-                else
-                    result -= buf;
+                else{
+                    switch(op){
+                        case "\\+" -> result += buf;
+                        case "-" -> result -= buf;
+                        case "\\*" -> result *= buf;
+                    }
+                }
+
             }
         }
 
         return result;
     }
-
-    //multiply 처리
-    static public int calLevel3(String s) {
-        String[] parts = s.split(" \\* ");
-        int result = 0;
-        int buf;
-        boolean flag = true;
-
-        for(String p : parts){
-            try{
-                buf = Integer.parseInt(p.trim());
-                if(flag){
-                    result = buf;
-                    flag = false;
-                }
-                else
-                    result *= buf;
-
-            }catch(Exception e){
-                buf = calLevel2(p);
-                if(flag){
-                    result = buf;
-                    flag = false;
-                }
-                else
-                    result *= buf;
-            }
-        }
-
-        return result;
-    }
-
-
-
 
 }
